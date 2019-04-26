a568d62bc5d2f43efee11da7175993b437b17496
mikelreal96 <mikelreal96>
2019-04-26 12:23:39 +0100

    Initial commit

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+  if (isset($_POST['email'])) {
+
+    //Email information
+    $admin_email = "brad@edgeledger.net";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    //send email
+    mail($admin_email, "New Form Submission", $message. '
+    - ' . $phone, "Form:" . $email);
+    
+    header('Location: http://edgeledger.net/success.html');
+  }
